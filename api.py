d60ae7e45f5d722f6bf03e896170d1047cb1bcb1
joaopbarcellos <jpzbarcellos@gmail.com>
2024-10-29 14:29:54 -0300

    Comentando configurações da API desnecessárias

--- api.py
+++ api.py
@@ -10,18 +10,18 @@ import uuid
 
 app = FastAPI()
 
-options = [
-    'http:localhost',
-    "http://127.0.0.1:5500"
-]
+# options = [
+#     'http:localhost',
+#     "http://127.0.0.1:5500"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=options,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=options,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 
 load_dotenv()
 
